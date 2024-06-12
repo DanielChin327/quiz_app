53d6c5c2f13b42174327ec04e63da03f9159cffb
Daniel Chin <DanChin327@gmail.com>
2024-06-12 15:08:37 -0600

    created JS and added list of questions.

---
+++ script.js
@@ -0,0 +1,38 @@
+const questions = [
+  {
+      question: "Which is largest animal in the world? ",
+      answers: [
+        {text: "Shark", correct: false},
+        {text: "Blue Whale", correct: true},
+        {text: "Elephant", correct: false},
+        {text: "Giraffe", correct: false},
+      ]
+  },
+  {
+    question: "Which is largest animal in the world? ",
+    answers: [
+      {text: "Shark", correct: false},
+      {text: "Blue Whale", correct: true},
+      {text: "Elephant", correct: false},
+      {text: "Giraffe", correct: false},
+    ]
+  },
+  {
+    question: "Which is largest animal in the world? ",
+    answers: [
+      {text: "Shark", correct: false},
+      {text: "Blue Whale", correct: true},
+      {text: "Elephant", correct: false},
+      {text: "Giraffe", correct: false},
+    ]
+  },
+  {
+    question: "Which is largest animal in the world? ",
+    answers: [
+      {text: "Shark", correct: false},
+      {text: "Blue Whale", correct: true},
+      {text: "Elephant", correct: false},
+      {text: "Giraffe", correct: false},
+    ]
+  },
+];
